3ece256a948c6471baf7a513b0e05778b6cc8fd8
naemcivic <naemcivic@gmail.com>
2016-09-12 15:43:53 -0400

    add a component to the app and modify ng module file to imort the component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from ''
 
 @NgModule ({
-    imports: [ BrowserModule ]
+    imports: [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
 })
 
-export class AppModule {}
+export class AppModule { }
